8aa93e04518908795087706f7fc59c0b0085bdd0
VBDP <vbd@vbd.es>
2025-07-18 11:10:49 +0200

    Finalizado capítulo 3 sobre Variables, Iniciando capítulo 4 sobre Data Types

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,14 +15,38 @@ public class Main {
 	 * pruebas con variables
 	 */
 	
-	public String prueba = "Prueba";
-	int numero1 = 1;
-	final int numero2 = 2;
+	
 	
 	public static void main(String[] args) {
+		
+		String prueba = "Output de un String";
+		int numero1 = 1;
+		final int numero2 = 2;
+		float numero3= 3.14f;
+		float numero4 = 6.18f;
+		boolean truefalse = false;
+		
+		//Declarando multiples variables con y sin valor
+		int x,y,z;
+		int a=1, b=2,c=3;
+		
+			//Text Outputs
+		System.out.println("Pruebas con Strings:");
 		System.out.println("Hello World!");
 		System.out.println("I am learning Java.");
 		System.out.println("It is awesome!");
+		System.out.println("=========================================");
+		//Number Outputs
+		System.out.println("Pruebas con numeros");
+		System.out.println(1);
+		System.out.println(4.50);
+		System.out.println("=========================================");
+		System.out.println("Pruebas con variables");
+		System.out.println(prueba);
+		System.out.println("Suma de dos enteros = " + (numero1 + numero2));
+		System.out.println("Suma de dos floats = " + (numero3 + numero4));
+		System.out.println("Output de un Boolean: " + truefalse);
+		System.out.println("Output de tres variables declaradas en una linea: " + a + b + c);
+		System.out.println("=========================================");
 	}
-
 }
