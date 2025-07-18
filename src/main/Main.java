5811c676d45f30bb12c376df3572fb8afd31986e
VBDP <vbd@vbd.es>
2025-07-18 10:48:15 +0200

    Inicializando el proyecto, Completados capítulos 1 y 2 sobre Introducción y Output, actualmente capítulo 3 sobre Variables

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Este mini script de java son pruebas de java del curso de
+ * w3schools. Con esto pretendo repasar y hacer mis proyectos 
+ * para adquirir conocimientos y experiencia.
+ */
+
+package main;
+
+public class Main {
+	
+	//Esto es un comentário
+	
+	/*
+	 * Esto es un comentário largo, lo de abajo son
+	 * pruebas con variables
+	 */
+	
+	public String prueba = "Prueba";
+	int numero1 = 1;
+	final int numero2 = 2;
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("I am learning Java.");
+		System.out.println("It is awesome!");
+	}
+
+}
